3fc598025afc7e90abd795f07fb712c413dcfa80
Gurnoor Singh Saini <sainigurnoor511@gmail.com>
2023-07-15 23:32:49 +0530

    Revert "Sell car GUI"
    This reverts commit 9d6531b5cca1ac8ad6619a16b83ad9282d46aea5.

--- buy_car_page.py
+++ buy_car_page.py
@@ -1,66 +1,46 @@
-import sv_ttk
-import tkinter
+from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
+
+# import sv_ttk
+# sv_ttk.set_theme("light")
 
 
 class BuyCarPage:
 
     def __init__(self):
-        self.root = tkinter.Tk()
+        self.root = Tk()
         self.root.geometry("1000x700")
         self.root.title("CarOBar -- Buy Car")
-        self.root.resizable(width=False, height=False)
-        self.root.configure(bg='#fff')
-        sv_ttk.set_theme("dark")
+        self.root.resizable(width =False, height= False)
+        self.root.configure(bg ='#fff')
 
     def buy_car_page_widgets(self):
 
-        self.frame = tkinter.Frame(self.root, width=1000, height=700)
-        self.frame.place(x=0, y=0)
+        self.frame = Frame(self.root,width=1000,height=700,bg='#c5d0e0')
+        self.frame.place(x=0,y=0)
 
-        self.heading = ttk.Label(self.frame, text='buy car', foreground='#c5d0e0', font=('Harlow Solid Italic', 40, 'normal'))
-        self.heading.place(x=420, y=5)
+        self.heading = Label(self.frame,text ='buy car' , fg='black',bg = '#c5d0e0',font=('Harlow Solid Italic',40,'normal'))
+        self.heading.place(x=420,y=5)
 
-        #!_________________________________________________________________________________________________________
-
-        self.brand = ttk.Label(self.frame,text='Select Brand', width=27, foreground='#c5d0e0', font=('Harlow Solid Italic', 16, 'normal'))
-        self.brand.place(x=80, y=110)
-
-        self.selectbrand = ttk.Combobox(self.root, width=27, textvariable="Select Brand")
-        self.selectbrand['values'] = (  ' Maruti', 
-                                        ' Mustang', 
-                                        ' Shelby', 
-                                        ' Honda', 
-                                        ' Renualt', 
-                                        ' Hyundai',)
-
-        self.selectbrand.place(x=80, y=140)
-        self.selectbrand.current()
-
-        #!_________________________________________________________________________________________________________
-
-        self.budget = ttk.Label(self.frame,text='Select Budget', width=27, foreground='#c5d0e0', font=('Harlow Solid Italic', 16, 'normal'))
-        self.budget.place(x=398, y=110)
-
-        self.selectbudget = ttk.Combobox(self.root, width=27, textvariable="Select Brand")
-        self.selectbudget['values'] = ( '0 - 3,00,000', 
-                                        '3,00,000 - 7,00,000', 
-                                        'Above 7,00,000')
-
-        self.selectbudget.place(x=398, y=140)
-        self.selectbudget.current()
-
-        #!_________________________________________________________________________________________________________
-
-        self.new_used = ttk.Label(self.frame,text='New / Used', width=27, foreground='#c5d0e0', font=('Harlow Solid Italic', 16, 'normal'))
-        self.new_used.place(x=705, y=110)
-
-        self.select_new_used = ttk.Combobox(self.root, width=27, textvariable="Select Brand")
-        self.select_new_used['values'] = ('New ','Used')
-        self.select_new_used.place(x=705, y=140)
-        self.select_new_used.current()
-
+        self.monthchoosen = ttk.Combobox(self.root, width = 27, textvariable= "Select Brand")
+  
+        # Adding combobox drop down list
+        self.monthchoosen['values'] = (' January', 
+                                ' February',
+                                ' March',
+                                ' April',
+                                ' May',
+                                ' June',
+                                ' July',
+                                ' August',
+                                ' September',
+                                ' October',
+                                ' November',
+                                ' December')
+  
+        self.monthchoosen.grid(column = 1, row = 5)
+        self.monthchoosen.current()
 
 if __name__ == "__main__":
     sell = BuyCarPage()
