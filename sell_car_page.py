09df14b2d1d56c713c92dabc9ddd51148a1a6294
Abhishek <xtractwings@gmail.com>
2023-07-25 21:43:24 +0530

    Update sell car page

--- sell_car_page.py
+++ sell_car_page.py
@@ -34,90 +34,90 @@ class SellCarPage:
 
     ###---------------------------------/////// SELL CAR DETAILS //////------------------------------###
 
-        self.sellcar_frame = Frame(self.root, width=1000,height=1000, bg= "#191970")
+        self.sellcar_frame = Frame(self.root, width=1000,height=1000, bg= "white")
         self.sellcar_frame.place(x=0,y=0)
 
-        self.heading = ttk.Label(self.sellcar_frame, text='Sell car', foreground='white',background="#191970", font=('Harlow Solid Italic', 40, 'normal'))
+        self.heading = ttk.Label(self.sellcar_frame, text='Sell car', foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 35, 'normal'))
         self.heading.place(x=420, y=0)
 
-        self.title1 = ttk.Label(self.sellcar_frame, text='----- Enter Car Details -----', foreground='white',background="#191970", font=('Harlow Solid Italic', 19, 'normal'))
-        self.title1.place(x=370, y=70)
+        self.title1 = ttk.Label(self.sellcar_frame, text='----- Enter Car Details -----', foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 19, 'normal'))
+        self.title1.place(x=370, y=90)
 
-        self.car_brand = ttk.Label(self.sellcar_frame,text="Car Brand", foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
-        self.car_brand.place(x=90, y=150,width=180, height=30)
+        self.car_brand = ttk.Label(self.sellcar_frame,text="Car Brand", foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
+        self.car_brand.place(x=90, y=160,width=180, height=30)
 
         self.car_brand_entry = ttk.Entry(self.sellcar_frame, font =20 )
-        self.car_brand_entry.place(x =220,y =150,width =180,height=30)
+        self.car_brand_entry.place(x =220,y =160,width =180,height=30)
 
 
-        self.car_model = ttk.Label(self.sellcar_frame,text="Car Model", foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
-        self.car_model.place(x=90,y=220,width=250, height=30)
+        self.car_model = ttk.Label(self.sellcar_frame,text="Car Model", foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
+        self.car_model.place(x=90,y=230,width=250, height=30)
 
         self.car_model_entry= ttk.Entry(self.sellcar_frame, font =20)
-        self.car_model_entry.place(x =220,y =220,width =180,height=30)
+        self.car_model_entry.place(x =220,y =230,width =180,height=30)
 
         # -------------/////////  COMBOBOXES //////////----------------#
 
-        self.car_reg = ttk.Label(self.sellcar_frame,text="Registration Year", foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
-        self.car_reg.place(x=520,y=150,width =250,height=30)
+        self.car_reg = ttk.Label(self.sellcar_frame,text="Registration Year", foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
+        self.car_reg.place(x=520,y=160,width =250,height=30)
 
         year =[2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000,1999,1998,1997,1996,1995,1994,1993,1992,1991,1990]
         self.car_reg_cb = ttk.Combobox(self.sellcar_frame, values=year)
-        self.car_reg_cb.place(x=720, y=150, width=180, height=30)
+        self.car_reg_cb.place(x=720, y=160, width=180, height=30)
         self.car_reg_cb["state"]='readonly'
         self.car_reg_cb.set("Select Year")
 
 
-        self.car_var = ttk.Label(self.sellcar_frame,text="Car Variant", foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
-        self.car_var.place(x=570, y=220, width=180, height=30)
+        self.car_var = ttk.Label(self.sellcar_frame,text="Car Variant", foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
+        self.car_var.place(x=570, y=230, width=180, height=30)
 
         variant =["Petrol","Diesel","Electric","Hybrid"]
         self.car_var_cb = ttk.Combobox(self.sellcar_frame, values=variant)
-        self.car_var_cb.place(x=720, y=220, width=180, height=30)
+        self.car_var_cb.place(x=720, y=230, width=180, height=30)
         self.car_var_cb["state"]='readonly'
         self.car_var_cb.set("Select Variant")
 
 
-        self.car_ownership = ttk.Label(self.sellcar_frame,text="Car Owner", foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
-        self.car_ownership.place(x=90,y=290,width=180,height=30)
+        self.car_ownership = ttk.Label(self.sellcar_frame,text="Car Owner", foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
+        self.car_ownership.place(x=90,y=300,width=180,height=30)
 
         ownership =["1st owner","2nd owner","3rd owner","4th owner","5th owner"]
         self.car_ownership_cb = ttk.Combobox(self.sellcar_frame, values=ownership)
-        self.car_ownership_cb.place(x =220,y =290,width =180,height=30)
+        self.car_ownership_cb.place(x =220,y =300,width =180,height=30)
         self.car_ownership_cb["state"]='readonly'
         self.car_ownership_cb.set("Select Ownership")
 
 
-        self.km_driven = ttk.Label(self.sellcar_frame,text="Km Driven", foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
-        self.km_driven.place(x=580,y=290,width =180,height=30)
+        self.km_driven = ttk.Label(self.sellcar_frame,text="Km Driven", foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
+        self.km_driven.place(x=580,y=300,width =180,height=30)
 
         odometer =["0 km - 10,000 km","10,000 km - 20,000 km","20,000 km - 30,000 km","30,000 km - 40,000 km","40,000 km - 50,000 km","50,000 km - 60,000 km","60,000 km - 70,000 km","70,000 km - 80,000 km","80,000 km - 90,000 km","90,000 km - 1,00,000 km","1,00,000 km - 1,10,000 km"]
         self.km_driven_cb = ttk.Combobox(self.sellcar_frame, values=odometer)
-        self.km_driven_cb.place(x =720,y =290,width =180,height=30)
+        self.km_driven_cb.place(x =720,y =300,width =180,height=30)
         self.km_driven_cb["state"]='readonly'
         self.km_driven_cb.set("odometer")
 
       #####---------------------------///// SELLER DETAILS  //////--------------------------------------------#####
 
-        self.title2_label = Label(self.sellcar_frame, text="----- Enter Seller Details -----",foreground='white',background="#191970", font=('Harlow Solid Italic', 19, 'normal'))
+        self.title2_label = Label(self.sellcar_frame, text="----- Enter Seller Details -----",foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 19, 'normal'))
         self.title2_label.place(x=345,y=400)
 
 
-        self.seller_name = ttk.Label(self.sellcar_frame,text="Seller Name",foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
+        self.seller_name = ttk.Label(self.sellcar_frame,text="Seller Name",foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
         self.seller_name.place(x=70,y=480,width=180, height=30)
 
         self.seller_name_entry = ttk.Entry(self.sellcar_frame, font =20 )
         self.seller_name_entry.place(x =220,y =480,width =180,height=30) 
 
 
-        self.seller_address = ttk.Label(self.sellcar_frame,text="Seller Address",foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
+        self.seller_address = ttk.Label(self.sellcar_frame,text="Seller Address",foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
         self.seller_address.place(x=50,y=560,width=180, height=30)
 
         self.seller_address_entry = ttk.Entry(self.sellcar_frame, font =20 )
         self.seller_address_entry.place(x =220,y =560,width =180,height=30) 
 
 
-        self.seller_mobile = ttk.Label(self.sellcar_frame,text="Seller Contact No.",foreground='white',background="#191970", font=('Harlow Solid Italic', 16, 'normal'))
+        self.seller_mobile = ttk.Label(self.sellcar_frame,text="Seller Contact No.",foreground='#57A1F8',background="white", font=('Harlow Solid Italic', 16, 'normal'))
         self.seller_mobile.place(x=520,y=480,width=180, height=30)
 
         self.seller_mobile_entry = ttk.Entry(self.sellcar_frame, font =20 )
@@ -127,8 +127,11 @@ class SellCarPage:
         self.user_image_path = Image.open('images\mainpage\submit_sellcar_button.png').resize((120,30))
         self.user_imageTk2 = ImageTk.PhotoImage(self.user_image_path)
         print(self.user_image_path)
-        self.sign_up = Button(self.sellcar_frame, image= self.user_imageTk2,borderwidth=0,background="#191970", command=self.get_sell_car_data)
+        self.sign_up = Button(self.sellcar_frame, image= self.user_imageTk2,borderwidth=0,background="white", command=self.get_sell_car_data)
         self.sign_up.place(x=440,y=640)
+
+        # self.submit = Button(self.root,width=12,text='submit',bg="#57A1F8",fg="white",command= self.get_sell_car_data)
+        # self.submit.place(x=440,y=640)
 
         self.root.mainloop()
         
@@ -242,7 +245,7 @@ class SellCarPage:
 
             ###----------------------//////// CONNECTING WITH DATABASE ///////-----------------------------#
 
-            result = database.update_car_and_seller_details(a)
+            result = database.update_car_and_seller_details(u)
             if result:
                     messagebox.showinfo("Message","Car & Seller details added successfully")
                     # self.root.destroy()
